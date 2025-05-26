b5e9c3d8d3d1ac62052a9fd7439d8dbaa775fa7b
tk-sawada <tak8.sawada@gmail.com>
2025-05-26 21:17:34 +0900

    delete error hundler

--- src/utils/getID.ts
+++ src/utils/getID.ts
@@ -1,13 +1,8 @@
 // utils/getID.ts
 
 export function getID(biourl: string): string {
-    try {
-      const url = new URL(biourl);
-      const segments = url.pathname.split("/").filter(Boolean);
-      return segments[0] ?? "";
-    } catch (e: unknown) {
-      console.error('Error caught in getID.ts:', e);
-      return "";
-    }
-  }
+  const url = new URL(biourl);
+  const segments = url.pathname.split("/").filter(Boolean);
+  return segments[0] ?? "";
+}
   
